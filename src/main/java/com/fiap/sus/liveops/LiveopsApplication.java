522e5dd4ef263af9338b55af00364609e24e1bb5
Luíza O. Rosa <lu.oliveira0505@gmail.com>
2026-02-03 01:19:22 -0300

    initial commit

---
+++ src/main/java/com/fiap/sus/liveops/LiveopsApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.sus.liveops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/sus/liveops/LiveopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.sus.liveops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
