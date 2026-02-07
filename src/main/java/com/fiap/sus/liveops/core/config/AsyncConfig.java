b7f454f70b6c2103c7f70fb45b81355507b70d34
Luíza O. Rosa <lu.oliveira0505@gmail.com>
2026-02-07 10:00:03 -0300

    chore: add async config

---
+++ src/main/java/com/fiap/sus/liveops/core/config/AsyncConfig.java
@@ -0,0 +1,45 @@
+package com.fiap.sus.liveops.core.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Slf4j
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    @Bean(name = "taskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AnalyticsAsync-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            log.error("Async error in method: {}", method.getName());
+
+            for (Object param : params) {
+                log.error("  - Param: {}", param);
+            }
+
+            log.error("Async Exception Stacktrace: ", ex);
+        };
+    }
+
+}
