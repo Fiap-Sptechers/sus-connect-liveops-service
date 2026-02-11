ec78bcf876768814557b5b2fba47d4a6af5bc056
Luíza O. Rosa <lu.oliveira0505@gmail.com>
2026-02-11 18:52:41 -0300

    fix: fix tests

--- src/test/java/com/fiap/sus/liveops/modules/attendance/controller/AttendanceControllerTest.java
+++ src/test/java/com/fiap/sus/liveops/modules/attendance/controller/AttendanceControllerTest.java
@@ -13,9 +13,11 @@ import com.fiap.sus.liveops.modules.attendance.mapper.AttendanceMapper;
 import com.fiap.sus.liveops.modules.attendance.service.AttendanceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,8 +29,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(AttendanceController.class)
+@WebMvcTest(controllers = AttendanceController.class, excludeAutoConfiguration = MongoAutoConfiguration.class)
 @AutoConfigureMockMvc(addFilters = false)
+@TestPropertySource(properties = {"mongock.enabled=false"})
 class AttendanceControllerTest {
 
     @Autowired
